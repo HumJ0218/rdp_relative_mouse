c2877c583bc5000ea7dfc79934a06e3260451f59
HumphreyJ <tlos142857@live.cn>
2024-07-19 16:36:27 +0800

    add console message

--- MouseLoop.cpp
+++ MouseLoop.cpp
@@ -18,7 +18,7 @@ LRESULT CALLBACK hookMouseLoop(int nCode, WPARAM wParam, LPARAM lParam)
         return CallNextHookEx(NULL, nCode, wParam, lParam);
     }
 
-    MSLLHOOKSTRUCT *pMouseStruct = reinterpret_cast<MSLLHOOKSTRUCT *>(lParam);
+    MSLLHOOKSTRUCT* pMouseStruct = reinterpret_cast<MSLLHOOKSTRUCT*>(lParam);
     int x = pMouseStruct->pt.x;
     int y = pMouseStruct->pt.y;
 
@@ -28,22 +28,30 @@ LRESULT CALLBACK hookMouseLoop(int nCode, WPARAM wParam, LPARAM lParam)
 
     if (x < boundaryLoopThreshold)
     {
+        printf("left, ");
+
         cross = true;
         crossedX = screenMetric.xDeviceSize() - boundaryLoopThreshold * 2;
     }
     else if (x > screenMetric.xDeviceSize() - boundaryLoopThreshold)
     {
+        printf("right, ");
+
         cross = true;
         crossedX = boundaryLoopThreshold * 2;
     }
 
     if (y < boundaryLoopThreshold)
     {
+        printf("top, ");
+
         cross = true;
         crossedY = screenMetric.yDeviceSize() - boundaryLoopThreshold * 2;
     }
     else if (y > screenMetric.yDeviceSize() - boundaryLoopThreshold)
     {
+        printf("bottom, ");
+
         cross = true;
         crossedY = boundaryLoopThreshold * 2;
     }
@@ -52,21 +60,25 @@ LRESULT CALLBACK hookMouseLoop(int nCode, WPARAM wParam, LPARAM lParam)
     {
         SetCursorPos(screenMetric.xDeviceToSystem(crossedX), screenMetric.yDeviceToSystem(crossedY));
         // block the original mouse move, otherwise the cursor will be moved back
+        printf("%d,%d -> %d,%d\n", x, y, crossedX, crossedY);
         return 1;
     }
 
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 
     HHOOK hMouseHook = SetWindowsHookEx(WH_MOUSE_LL, hookMouseLoop, NULL, 0);
 
     if (hMouseHook == NULL)
     {
-        MessageBox(NULL, "Failed to install mouse hook", "Error", MB_ICONERROR);
+        printf("Failed to install mouse hook\n");
         return 1;
+    }
+    else {
+        printf("ready\n");
     }
 
     MSG msg;
